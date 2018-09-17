97931b9d65ea8e97c52cbbf596d46607f016a07e
Imran Ashraf <iimran.aashraf@gmail.com>
2018-09-17 14:36:15 +0500

    updated package name/path

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/mymath'))
+import mymath
+# sys.path.insert(0, os.path.abspath('../src/mymath'))
 
 
 # -- Project information -----------------------------------------------------

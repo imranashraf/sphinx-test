a332ac62ae069e7092caed8708dd6fa563f1387b
Imran Ashraf <iimran.aashraf@gmail.com>
2018-09-17 08:25:03 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='MyMath',
+      version='0.1',
+      description='Pakcage Test',
+      url='http://github.com',
+      author='I. Ashraf',
+      author_email='i.ashraf@example.com',
+      license='MIT',
+      packages=['mymath'],
+      package_dir={'': 'src'},
+      zip_safe=False)
